4cb06b34e1d15730f4acb14ab9f41dfd030b1dee
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-03-25 17:50:31 +0800

    Update cert.sh

--- v2ray/cert.sh
+++ v2ray/cert.sh
@@ -4,29 +4,35 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo "Checking VPS"
+# Getting
 clear
+echo start
+sleep 0.5
+source /var/lib/premium-script/ipvps.conf
+domain=$IP
+sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
+cd /root/
+wget -O acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
+bash acme.sh --install
+rm acme.sh
+cd .acme.sh
+echo "starting...., Port 80 Akan di Hentikan Saat Proses install Cert"
+bash acme.sh --register-account -m patajunit@gmail.com
+bash acme.sh --issue --standalone -d $domain --force
+bash acme.sh --installcert -d $domain
+--fullchainpath /etc/v2ray/v2ray.crt
+--keypath /etc/v2ray/v2ray.key
 clear
-echo -e "[ ${green}INFO${NC} ] Start " 
+echo start
 sleep 0.5
-domain=$(cat /var/lib/premium-script/ipvps.conf | cut -d'=' -f2)
-Cek=$(lsof -i:80 | cut -d' ' -f1 | awk 'NR==2 {print $1}')
-if [[ ! -z "$Cek" ]]; then
-sleep 1
-echo -e "[ ${red}WARNING${NC} ] Detected port 80 used by $Cek " 
-systemctl stop $Cek
-sleep 2
-echo -e "[ ${green}INFO${NC} ] Processing to stop $Cek " 
-sleep 1
-fi
-echo -e "[ ${green}INFO${NC} ] Starting renew cert... " 
-sleep 2
-/root/.acme.sh/acme.sh --issue -d $domain --debug --force --standalone --keylength ec-256
-echo -e "[ ${green}INFO${NC} ] Renew cert done... " 
-sleep 2
-echo -e "[ ${green}INFO${NC} ] Starting service $Cek " 
-sleep 2
-sed -i "s/$domainlama/$dom
-echo $domain > /etc/v2ray/domain
-echo -e "[ ${green}INFO${NC} ] All finished... " 
+source /var/lib/premium-script/ipvps.conf
+domain=$IP
+systemctl stop v2ray
+systemctl stop v2ray@none
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
+systemctl start v2ray
+systemctl start v2ray@none
+echo Done
 sleep 0.5
-echo ""
+clear 
